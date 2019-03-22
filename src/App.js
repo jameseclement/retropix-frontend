95d4b104ef0dd867d2633cff35758d7f25b2e085
James Clement <jameseclement@gmail.com>
2019-03-22 16:25:52 -0400

    add Nav component and put it in App

--- src/App.js
+++ src/App.js
@@ -6,36 +6,41 @@ import Adapter from "./Adapter";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      user: {id: 1},
+      user: { id: 1 },
       doc_id: 1,
       doc: {},
       version: {},
       tool: null,
       color: null,
       size: null
-    }
+    };
   }
 
   componentDidMount() {
     // this.loadUser()
-    this.loadDoc()
+    this.loadDoc();
   }
 
   loadUser() {
     this.setState({
-      user: {id: 1}
-    })
+      user: { id: 1 }
+    });
   }
 
   loadDoc(id) {
-    Adapter.getDoc(this.state.user.id, this.state.doc_id)
-      .then(doc => this.setState({doc}))
+    Adapter.getDoc(this.state.user.id, this.state.doc_id).then(doc =>
+      this.setState({ doc })
+    );
   }
 
   render() {
-    return <Main doc={this.state.doc} />;
+    return;
+    <div>
+      <Nav />
+      <Main doc={this.state.doc} />
+    </div>;
   }
 }
 
