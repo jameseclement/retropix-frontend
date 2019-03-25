621880ada28196688255f56a8627a0c9e0183182
Ben Yellin <byellindc@gmail.com>
2019-03-25 09:24:06 -0400

    switch tool sidebar to custom markup

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -25,30 +25,21 @@ class Demo extends Component {
       </Menu>
       <Grid className="">
         <Grid.Column width={2}>
-          <Sidebar.Pushable as={Segment} className="tools-sidebar">
-            <Sidebar as={Menu} animation='overlay' className="tools" icon='labeled' inverted vertical visible width='thin'>
-              <Menu.Item as='a' className="tool tool-pencil">
-                <img src={require('../icons/pencil.png')} alt="pencil" />
-              </Menu.Item>
-              <Menu.Item as='a' className="tool tool-eraser">
-                <img src={require('../icons/eraser.png')} alt="eraser" />
-              </Menu.Item>
-              <Menu.Item as='a' className="tool tool-line">
-                <img src={require('../icons/line.png')} alt="line" />
-              </Menu.Item>
-            </Sidebar>
-
-            <Sidebar.Pusher>
-              <Segment basic>
-                <Header as='h3'>Application Content</Header>
-                <Image src='/images/wireframe/paragraph.png' />
-              </Segment>
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <ThemingLayout />
-          </Grid.Column>
+          <div className="sidebar tools">
+            <a href="#" className="tool tool-pencil">
+              <img src={require('../icons/pencil.png')} alt="pencil" />
+            </a>
+            <a href="#" className="tool tool-eraser">
+              <img src={require('../icons/eraser.png')} alt="eraser" />
+            </a>
+            <a href="#" className="tool tool-line">
+              <img src={require('../icons/line.png')} alt="line" />
+            </a>
+          </div>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <ThemingLayout />
+        </Grid.Column>
       </Grid>
     </div>
   }
