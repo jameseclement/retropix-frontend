0b56f2d809686ee5a262bdba541157b689df5c36
Ben Yellin <byellindc@gmail.com>
2019-03-26 10:04:19 -0400

    update Main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {SketchField, Tools} from 'react-sketch';
 import Canvas from "../components/Canvas";
-// import Modal from "./Modal";
 
 class Main extends Component {
   constructor() {
@@ -25,15 +24,6 @@ class Main extends Component {
       />
     );
   }
-      // <Canvas
-      //   doc={this.props.doc}
-      //   tool={this.props.tool}
-      //   color={this.props.color}
-      //   size={this.props.size}
-      //   width={700}
-      //   height={400}
-      //   handleSave={this.props.handleSave}
-      // />
 }
 
 export default Main;
