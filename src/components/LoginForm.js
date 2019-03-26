f437a043730689291950ebedfbdae5c76a847667
James Clement <jameseclement@gmail.com>
2019-03-26 08:10:46 -0400

    add cards to docscontainer, add loginform, add signup form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-
+import { Button, Form } from "semantic-ui-react";
 class LoginForm extends Component {
   render() {
-    return <div>This is a login form</div>;
+    return (
+      <Form>
+        <Form.Field>
+          <label>Create Username</label>
+          <input placeholder="UserName" />
+        </Form.Field>
+        <Form.Field>
+          <label>Create Password</label>
+          <input type="password" placeholder="Password" />
+        </Form.Field>
+        <Form.Field>
+          <label>Verify Password</label>
+          <input type="password" placeholder="Password" />
+        </Form.Field>
+        <Button type="submit">Create User</Button>
+      </Form>
+    );
   }
 }
 
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
 
 class SignupForm extends Component {
   render() {
-    return <div>This is a Signup form</div>;
+    return (
+      <Form>
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder="UserName" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
   }
 }
 
